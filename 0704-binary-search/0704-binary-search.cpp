95a0890a790f2337c59eaa203b54c25406ccc45e
Adina Ather <adinaather692@gmail.com>
2026-08-12 00:24:42 +0500

    Time: 0 ms (100%), Space: 31.3 MB (46.95%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int st = 0, end = nums.size() - 1;
-        
-        while (st <= end) {
-            // Using this formula avoids potential integer overflow
-            int mid = st + (end - st) / 2;
-            
-            if (target > nums[mid]) {
-                st = mid + 1;
-            } else if (target < nums[mid]) {
-                end = mid - 1;
+    //helper
+    int binSearch(vector<int>& arr, int tar, int st, int end) {
+        if(st <= end) {
+            int mid = st + (end-st)/2;
+
+            if(arr[mid] == tar) return mid;
+            else if(arr[mid] <= tar) {
+                return binSearch(arr, tar, mid+1, end);
             } else {
-                return mid; // Target found
+                return binSearch(arr, tar, st, mid-1);
             }
         }
-        
-        return -1; // Target not found
+
+        return -1;
+    }
+
+    int search(vector<int>& arr, int tar) {
+        return binSearch(arr, tar, 0, arr.size()-1);
     }
 };
+//this one is the recursive approach of a binary search
